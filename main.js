b7ad4732bed775de440967ee0b0679ac08c30201
badutmemori <reyandreyt@gmail.com>
2022-07-15 18:45:59 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const currentyear = new Date().getFullYear();
+const currentdate = new Date().getDate();
+
+const audio = document.getElementById('audio');
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countDown = document.getElementById('countdown');
+const currentYear = new Date().getFullYear();
+const newYearTime = new Date(`July 15 ${currentYear} 13:54:00`);
+document.getElementById('year').innerHTML = 'Hello Yuchi ^_^';
+// document.getElementById('currentyear').innerHTML = currentYear + 1;
+
+function play() {
+  audio.play();
+  audio.loop = true;
+}
+
+function updateCountDown() {
+  const currentTime = new Date();
+  diff = newYearTime - currentTime;
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
+
+  if (diff < 0) {
+    window.location = 'birthday.html';
+  }
+}
+setInterval(updateCountDown, 1000);
