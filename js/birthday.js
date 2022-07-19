756e4fed272599d87b19ecfef098f5ce57cca7d7
badutmemori <reyandreyt@gmail.com>
2022-07-19 13:42:58 +0700

    adjust

---
+++ js/birthday.js
@@ -0,0 +1,29 @@
+var musik = 'Pesawat Kertas 365 Hari - JKT 48 (Acoustic Version by Sisca) Lirik _ hidup bagaikan pesawat kertas.mp3';
+
+AOS.init({ once: true });
+
+var audio = document.querySelector('.audio');
+if (musik) {
+  audio.src = musik;
+}
+function mulai() {
+  audio.play();
+  document.querySelector('.open').style = 'opacity: 0;';
+  document.querySelector('.body').style = 'overflow-y: scroll;';
+  setTimeout(function () {
+    document.querySelector('.open').style.display = 'none';
+  }, 1000);
+}
+function wa(isi) {
+  window.open('https://api.whatsapp.com/send?text= Makasih ya udah inget ulang tahun aku, di hari spesial ini aku pengin ' + isi);
+}
+async function tanya() {
+  var { value: kado } = await swal.fire({ title: 'Di hari spesial ini kamu pengin nya apa?', input: 'text', showCancelButton: false });
+  if (kado) {
+    await swal.fire('Kirim jawabannya ke wa aku ya');
+    wa(kado);
+  } else {
+    await swal.fire('Jangan dikosongin dong');
+    tanya();
+  }
+}
